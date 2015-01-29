a0dc3e43cdd47c5e888ff0181c9ad30c8f104892
liujing <liujing1@wepiao.com>
2015-01-29 23:53:33 +0800

    add collision detection

--- js/common.js
+++ js/common.js
@@ -36,3 +36,26 @@ function d2a(deg){
 function a2d(arc){
 	return arc*180/Math.PI;
 }
+
+function isNotInScreen(w, h, obj, padding){
+	if(obj.x< -padding || obj.x>w+padding || obj.y < -padding || obj.y > h+padding){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function testCircle(obj1, obj2){
+	var x = obj1.x - obj2.x;
+	var y = obj1.y - obj2.y;
+
+	var dis = Math.sqrt(x*x + y*y);
+
+
+	if(dis <= obj1.w/2+obj2.w/2){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
